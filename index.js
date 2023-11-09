a8f5fcec88f7a682f2b5d5e60676c1203cbe904d
ranggasuryap15 <ranggasurya.313@gmail.com>
2023-11-10 00:49:33 +0700

    mengatasi error CORS Origin

--- index.js
+++ index.js
@@ -3,6 +3,17 @@ const app = express();
 
 const productRoutes = require("./src/routes/products");
 
+// izinkan semua akses darimana pun
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, PATCH, DELETE"
+  );
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
+
 app.use("/", productRoutes);
 
 app.listen(4000, () => {
