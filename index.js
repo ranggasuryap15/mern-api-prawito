ce40a4f1aa9e2e026e82c6062906e44379e3080f
ranggasuryap15 <ranggasurya.313@gmail.com>
2023-11-09 22:39:51 +0700

    router

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
 const express = require("express");
 
 const app = express();
+const router = express.Router();
 
-app.use(() => {
-  console.log("We got a new request");
+// GET '/users' => [{name: 'John'}, {name: 'Jane'}]
+router.use("/users", (req, res, next) => {
+  res.json([{ name: "John" }, { name: "Jane" }]);
 });
+
+router.use("/price", (req, res, next) => {
+  res.json({ price: 10 });
+});
+
+router.get("/", (req, res, next) => {
+  res.json({ message: "Hello World" });
+});
+
+app.use("/", router);
 
 app.listen(4000, () => {
   console.log("Server is running on port 4000");
