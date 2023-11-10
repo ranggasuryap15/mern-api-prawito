abb907512a4b89d23246547a798e182b7c9d27ec
ranggasuryap15 <ranggasurya.313@gmail.com>
2023-11-10 23:37:40 +0700

    update blog

--- src/controllers/blog.js
+++ src/controllers/blog.js
@@ -74,3 +74,51 @@ exports.getBlogPostById = (req, res, next) => {
       next(err);
     });
 };
+
+exports.updateBlogPost = (req, res, next) => {
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    const error = new Error("invalid value");
+    error.errorStatus = 400;
+    error.data = errors.array();
+
+    throw error;
+  }
+
+  if (!req.file) {
+    const error = new Error("Image must be uploaded");
+    error.errorStatus = 422;
+    throw error;
+  }
+
+  const title = req.body.title;
+  const image = req.file.path;
+  const body = req.body.body;
+  const postId = req.params.id;
+
+  BlogPost.findById(postId)
+    .then((post) => {
+      if (!post) {
+        const error = new Error("Blog post tidak ditemukan");
+        error.errorStatus = 404;
+        throw error;
+      }
+
+      // update
+      post.title = title;
+      post.body = body;
+      post.image = image;
+
+      return post.save();
+    })
+    .then((result) => {
+      res.status(200).json({
+        message: "Update blog post berhasil",
+        data: result,
+      });
+    })
+    .catch((err) => {
+      next(err);
+    });
+};
--- src/routes/blog.js
+++ src/routes/blog.js
@@ -16,5 +16,16 @@ router.post(
 
 router.get("/posts", blogController.getAllBlogPost);
 router.get("/post/:id", blogController.getBlogPostById);
+router.put(
+  "/post/:id",
+  [
+    body("title")
+      .isLength({ min: 5 })
+      .withMessage("Input title minimal 5 huruf"),
+    body("body").isLength({ min: 5 }).withMessage("Input body minimal 5 huruf"),
+  ],
+  blogController.updateBlogPost
+);
+router.delete("/post/:id", blogController.deleteBlogPost);
 
 module.exports = router;
